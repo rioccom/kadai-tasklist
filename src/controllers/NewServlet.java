d259c8283f4950a1596182b34ace16e6fceea227
rioccom <373ryo0911@gmail.com>
2019-11-19 16:02:31 +0900

    modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 @WebServlet(name = "new", urlPatterns = { "/new" })
 public class NewServlet extends HttpServlet {
@@ -24,29 +22,14 @@ public class NewServlet extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EntityManager em = DBUtil.createEntityManager();
-		em.getTransaction().begin();
+		// ____CSRF対策___________________________________________________________
+		request.setAttribute("_token", request.getSession().getId());
 		
-		//____ Taskのインスタンスを生成____________________________
-		Task m = new Task();
+		// ____おまじないとしてのインスタンスを生成______________________________
+		request.setAttribute("task", new Task()); //リクエストスコープに(属性,値)を追加
 		
-		//____mのプロパティにデータを代入__________________________
-		String content = "hello";
-		m.setContent(content);
-		
-		//____現在の日時を取得_____________________________________
-		Timestamp currentTime = new Timestamp(System.currentTimeMillis());     
-		m.setCreated_at(currentTime);
-		m.setUpdated_at(currentTime);
-		
-		//____データベースに保存_____________________________________
-		em.persist(m);                      //DBに保存
-		em.getTransaction().commit();  //確定
-		
-		//____自動採番されたIDの値を表示________________________
-		response.getWriter().append(Integer.valueOf(m.getId()).toString());  //getWriter...サーブレットの処理結果として文字を出力
-		
-		em.close();
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp"); //new.jspにフォワード
+		rd.forward(request, response);
 	}
 	
 }
